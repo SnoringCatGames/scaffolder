a6ca856993e5234252b5dd3b269e399bd2b607ea
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-05-05 10:36:40 -0700

    Add a settings item for toggling welcome-panel

---
+++ src/gui/settings_items/WelcomePanelSettingsLabeledControlItem.gd
@@ -0,0 +1,25 @@
+class_name WelcomePanelSettingsLabeledControlItem
+extends CheckboxLabeledControlItem
+
+const LABEL := "Welcome panel"
+const DESCRIPTION := (
+        "The 'Welcome panel' is an overlay that is shown when starting a " +
+        "new level.")
+
+func _init(__ = null).(
+        LABEL,
+        DESCRIPTION \
+        ) -> void:
+    pass
+
+func on_pressed(pressed: bool) -> void:
+    Gs.is_welcome_panel_shown = pressed
+    Gs.save_state.set_setting(
+            Gs.IS_WELCOME_PANEL_SHOWN_SETTINGS_KEY,
+            Gs.is_welcome_panel_shown)
+
+func get_is_pressed() -> bool:
+    return Gs.is_welcome_panel_shown
+
+func get_is_enabled() -> bool:
+    return true
