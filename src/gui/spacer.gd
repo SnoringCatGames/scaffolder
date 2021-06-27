ea17eda94c271b410b7a45d639b98dce3842b752
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-06-26 22:17:53 -0700

    Create a custom Spacer scene, which can better handle screen resizing.

--- src/gui/spacer.gd
+++ src/gui/spacer.gd
@@ -6,12 +6,7 @@ extends Control
 export var size := Vector2.ZERO setget _set_size
 
 
-func update_gui_scale() -> void:
-    # FIXME: -----------------------------
-    # - Account for in-editor use
-    rect_min_size = size * Gs.gui.scale
-
-
 func _set_size(value: Vector2) -> void:
     size = value
-    update_gui_scale()
+    set_meta("gs_rect_min_size", size)
+    Gs.utils._scale_gui_recursively(self)
