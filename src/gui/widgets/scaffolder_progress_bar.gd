f0e0cd3123d326b53ec39f70f521dd76d76fc00c
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-06-27 13:51:19 -0700

    Create new ScaffolderProgressBar and ScaffolderSlider scenes.

---
+++ src/gui/widgets/scaffolder_progress_bar.gd
@@ -0,0 +1,12 @@
+tool
+class_name ScaffolderProgressBar, "res://addons/scaffolder/assets/images/editor_icons/progress_bar.png"
+extends ProgressBar
+
+
+export var size_override := Vector2.ZERO setget _set_size_override
+
+
+func _set_size_override(value: Vector2) -> void:
+    size_override = value
+    set_meta("gs_rect_min_size", size_override)
+    Gs.utils._scale_gui_recursively(self)
