541565a197b9af20079c35420264717fd7c0cc02
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-08-12 15:15:05 -0700

    Add support for configuring surface-side attachment with SpawnPosition; Move Time inner-classes out into separate files.

---
+++ src/utils/time/_timeout.gd
@@ -0,0 +1,36 @@
+tool
+class_name _Timeout
+extends Reference
+
+
+var time_tracker
+var elapsed_time_key: String
+var callback: FuncRef
+var time: float
+var arguments: Array
+var id: int
+
+
+func _init(
+        time_type: int,
+        callback: FuncRef,
+        delay: float,
+        arguments: Array) -> void:
+    self.time_tracker = Sc.time._get_time_tracker_for_time_type(time_type)
+    self.elapsed_time_key = \
+            Sc.time._get_elapsed_time_key_for_time_type(time_type)
+    self.callback = callback
+    self.time = time_tracker.get(elapsed_time_key) + delay
+    self.arguments = arguments
+    self.id = Sc.time.get_next_task_id()
+
+
+func get_has_expired() -> bool:
+    var elapsed_time: float = time_tracker.get(elapsed_time_key)
+    return elapsed_time >= time
+
+
+func trigger() -> void:
+    if !callback.is_valid():
+        return
+    callback.call_funcv(arguments)
