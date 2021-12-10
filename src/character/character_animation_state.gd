06b31693fe0de00fa167f5ea10bcd4e13d9814b2
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-12-09 23:51:26 -0800

    Remove references to the Surfacer framework from within the Scaffolder framework.

--- src/character/character_animation_state.gd
+++ src/character/character_animation_state.gd
@@ -7,8 +7,6 @@ const MIN_POST_PATH_CONFIDENCE_OPACITY := 0.3
 const LOW_CONFIDENCE_MODULATE_MASK := Color("cccc22")
 
 var character_position := Vector2.INF
-var grabbed_surface: Surface
-var grab_position: Vector2
 var animation_name := "Rest"
 var animation_position := 0.0
 var facing_left := false
@@ -17,8 +15,6 @@ var confidence_multiplier := 0.0
 
 func reset() -> void:
     self.character_position = Vector2.INF
-    self.grabbed_surface = null
-    self.grab_position = Vector2.INF
     self.animation_name = "Rest"
     self.animation_position = 0.0
     self.facing_left = false
@@ -27,8 +23,6 @@ func reset() -> void:
 
 func copy(other: CharacterAnimationState) -> void:
     self.character_position = other.character_position
-    self.grabbed_surface = other.grabbed_surface
-    self.grab_position = other.grab_position
     self.animation_name = other.animation_name
     self.animation_position = other.animation_position
     self.facing_left = other.facing_left
