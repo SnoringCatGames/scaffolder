22202294def66f31c3068c6e53ab79254617cee9
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-12-09 23:17:31 -0800

    Remove references to the Surfacer framework from within the Scaffolder framework.

--- src/utils/time/profiler.gd
+++ src/utils/time/profiler.gd
@@ -73,7 +73,8 @@ func stop_with_optional_metadata(
         thread_id := DEFAULT_THREAD_ID,
         records_profile_or_metadata_container = null) -> float:
     if records_profile_or_metadata_container != null:
-        if records_profile_or_metadata_container.timings != null:
+        if records_profile_or_metadata_container is Object and \
+                records_profile_or_metadata_container.timings != null:
             return stop(
                     metric,
                     thread_id,
