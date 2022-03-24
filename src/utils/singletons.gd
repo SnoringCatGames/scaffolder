319184a9192d1c89abbef30276c17f75f6c6193e
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-24 12:54:03 -0700

    Update Singletons to use set_meta/get_meta on an external singleton to prevent potential issues with Reference unloading.

--- src/utils/singletons.gd
+++ src/utils/singletons.gd
@@ -4,10 +4,14 @@ extends Reference
 
 
 # Dictionary<Script, Variant>
-const _SCRIPT_INSTANCES := {}
-
+const _SCRIPT_INSTANCES_KEY := "Singletons_SCRIPT_INSTANCES"
 # Dictionary<Variant, Variant>
-const _VALUES := {}
+const _VALUES_KEY := "Singletons_VALUES"
+
+# - This is a hack.
+# - But it is a safe way to ensure that our singletons registry is never
+#   unloaded as long as the app is running.
+const _KNOWN_EXTERNAL_SINGLETON := Engine
 
 
 static func instance(script_or_path):
@@ -17,21 +21,34 @@ static func instance(script_or_path):
     else:
         script = script_or_path
     
-    if _SCRIPT_INSTANCES.has(script):
-        return _SCRIPT_INSTANCES[script]
-    else:
+    var script_instances := _get_script_instances()
+    
+    if !script_instances.has(script):
         var instance = script.new()
-        _SCRIPT_INSTANCES[script] = instance
-        return instance
+        script_instances[script] = instance
+    
+    return script_instances[script]
 
 
 static func has_value(key) -> bool:
-    return _VALUES.has(key)
+    return _get_values().has(key)
 
 
 static func get_value(key):
-    return _VALUES[key]
+    return _get_values()[key]
 
 
 static func set_value(key, value) -> void:
-    _VALUES[key] = value
+    _get_values()[key] = value
+
+
+static func _get_script_instances() -> Dictionary:
+    if !_KNOWN_EXTERNAL_SINGLETON.has_meta(_SCRIPT_INSTANCES_KEY):
+        _KNOWN_EXTERNAL_SINGLETON.set_meta(_SCRIPT_INSTANCES_KEY, {})
+    return _KNOWN_EXTERNAL_SINGLETON.get_meta(_SCRIPT_INSTANCES_KEY)
+
+
+static func _get_values() -> Dictionary:
+    if !_KNOWN_EXTERNAL_SINGLETON.has_meta(_VALUES_KEY):
+        _KNOWN_EXTERNAL_SINGLETON.set_meta(_VALUES_KEY, {})
+    return _KNOWN_EXTERNAL_SINGLETON.get_meta(_VALUES_KEY)
