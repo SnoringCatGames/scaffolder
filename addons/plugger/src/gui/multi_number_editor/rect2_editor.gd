62250f631dc16dadfe820ae03e97ab371b8221e7
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-29 13:30:08 -0700

    Update MultNumberEditor and subclasses to support updating the control values from outside the UI.

--- addons/plugger/src/gui/multi_number_editor/rect2_editor.gd
+++ addons/plugger/src/gui/multi_number_editor/rect2_editor.gd
@@ -6,6 +6,9 @@ extends MultiNumberEditor
 signal value_changed(new_value)
 
 
+var value: Rect2 setget _set_value
+
+
 func _init() -> void:
     keys = ["x", "y", "width", "height"]
 
@@ -15,3 +18,13 @@ func _on_property_changed(value: float) -> void:
     var values := _get_values()
     var vector := Rect2(values.x, values.y, values.width, values.height)
     emit_signal("value_changed", vector)
+
+
+func _set_value(new_value: Rect2) -> void:
+    value = new_value
+    _set_values({
+        x = new_value.position.x,
+        y = new_value.position.y,
+        width = new_value.size.x,
+        height = new_value.size.y,
+    })
--- addons/plugger/src/gui/multi_number_editor/vector2_editor.gd
+++ addons/plugger/src/gui/multi_number_editor/vector2_editor.gd
@@ -6,6 +6,9 @@ extends MultiNumberEditor
 signal value_changed(new_value)
 
 
+var value: Vector2 setget _set_value
+
+
 func _init() -> void:
     keys = ["x", "y"]
 
@@ -15,3 +18,11 @@ func _on_property_changed(value: float) -> void:
     var values := _get_values()
     var vector := Vector2(values.x, values.y)
     emit_signal("value_changed", vector)
+
+
+func _set_value(new_value: Vector2) -> void:
+    value = new_value
+    _set_values({
+        x = new_value.x,
+        y = new_value.y,
+    })
