d1dcf8f67eab1761bbb05b9cbc9eaccf20373884
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-04-01 09:41:26 -0700

    Fix a bug with StaticColorConfig independent hsv component assignment.

--- src/config/color_config/static_color_config.gd
+++ src/config/color_config/static_color_config.gd
@@ -24,53 +24,65 @@ var g: float setget _set_g,_get_g
 var b: float setget _set_b,_get_b
 var color: Color setget _set_color,_get_color
 
-var _color := TRANSPARENT
+var _h := TRANSPARENT.h
+var _s := TRANSPARENT.s
+var _v := TRANSPARENT.v
+var _a := TRANSPARENT.a
 
 
 func sample(_redirect_depth := 0) -> Color:
-    return _color
+    return Color.from_hsv(_h, _s, _v, _a)
 
 
 func _set_h(value: float) -> void:
-    _color.h = value
+    _h = value
 func _set_s(value: float) -> void:
-    _color.s = value
+    _s = value
 func _set_v(value: float) -> void:
-    _color.v = value
+    _v = value
 func _set_a(value: float) -> void:
-    _color.a = value
+    _a = value
 
 func _set_r(value: float) -> void:
-    _color.r = value
+    var color = sample()
+    color.r = value
+    _set_color(color)
 func _set_g(value: float) -> void:
-    _color.g = value
+    var color = sample()
+    color.g = value
+    _set_color(color)
 func _set_b(value: float) -> void:
-    _color.b = value
+    var color = sample()
+    color.b = value
+    _set_color(color)
 
 func _get_h() -> float:
-    return _color.h
+    return _h
 func _get_s() -> float:
-    return _color.s
+    return _s
 func _get_v() -> float:
-    return _color.v
+    return _v
 func _get_a() -> float:
-    return _color.a
+    return _a
 
 func _get_r() -> float:
-    return _color.r
+    return sample().r
 func _get_g() -> float:
-    return _color.g
+    return sample().g
 func _get_b() -> float:
-    return _color.b
+    return sample().b
 
 func _set_color(value: Color) -> void:
-    _color = value
+    _h = value.h
+    _s = value.s
+    _v = value.v
+    _a = value.a
 func _get_color() -> Color:
-    return _color
+    return sample()
 
 
 func to_string() -> String:
-    return ("StaticColorConfig(%s)") % str(_color)
+    return ("StaticColorConfig(%s)") % str(sample())
 
 
 func _get_property_list() -> Array:
