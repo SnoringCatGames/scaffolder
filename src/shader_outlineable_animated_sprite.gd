56d5f38686f9d9e8638c5d2226b014d0b1a3f89e
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-04-25 14:31:53 -0700

    Create a new OutlineableAnimatedSprite.

---
+++ src/shader_outlineable_animated_sprite.gd
@@ -0,0 +1,86 @@
+tool
+class_name ShaderOutlineableAnimatedSprite, \
+"res://addons/scaffolder/assets/images/editor_icons/outlineable_animated_sprite.png"
+extends OutlineableAnimatedSprite
+## -   This supports toggling an outline around the sprite, and changing the[br]
+##     color of the outline.[br]
+## -   This implementation of OutlineableAnimatedSprite uses a shader to[br]
+##     render the outline.[br]
+## -   TextureOutlineableSprite is more efficient than[br]
+##     ShaderOutlineableSprite.[br]
+##     -   By a factor of around x7, for a one-pixel border.[br]
+## -   But using a separate outline texture is more work for the sprite[br]
+##     author.[br]
+## -   So shader-based outlines might be better for prototyping, or when you[br]
+##     know you won't be rendering too many outlined sprites simultaneously.[br]
+
+
+const PIXELATED_OUTLINE_SHADER := \
+        preload("res://addons/scaffolder/src/pixelated_outline.gdshader")
+const ANTIALIASED_OUTLINE_SHADER := \
+        preload("res://addons/scaffolder/src/antialiased_outline.gdshader")
+
+export var is_antialiased := false setget _set_is_antialiased
+
+export var thickness := 1.0 setget _set_thickness
+export(int, "Diamond", "Circle", "Square") var pattern := 2 setget _set_pattern
+export var is_inside := false setget _set_is_inside
+export var adds_margins := false setget _set_adds_margins
+
+var _outline_material: ShaderMaterial
+
+
+func _init() -> void:
+    _outline_material = ShaderMaterial.new()
+    _outline_material.shader = PIXELATED_OUTLINE_SHADER
+
+
+func _ready() -> void:
+    _update_outline()
+
+
+func _update_outline() -> void:
+    _outline_material.shader = ANTIALIASED_OUTLINE_SHADER
+    if is_outlined:
+        if is_antialiased:
+            _outline_material.shader = ANTIALIASED_OUTLINE_SHADER
+        else:
+            _outline_material.shader = PIXELATED_OUTLINE_SHADER
+            _outline_material.set_shader_param("pattern", pattern)
+            _outline_material.set_shader_param("is_inside", is_inside)
+            _outline_material.set_shader_param("adds_margins", adds_margins)
+        _outline_material.set_shader_param("color", outline_color)
+        _outline_material.set_shader_param("thickness", thickness)
+        self.material = _outline_material
+    else:
+        self.material = null
+
+
+func _set_is_antialiased(value: bool) -> void:
+    is_antialiased = value
+    _update_outline()
+
+
+func _set_outline_color(value: Color) -> void:
+    outline_color = value
+    _outline_material.set_shader_param("color", outline_color)
+
+
+func _set_thickness(value: float) -> void:
+    thickness = value
+    _outline_material.set_shader_param("thickness", thickness)
+
+
+func _set_pattern(value: int) -> void:
+    pattern = value
+    _outline_material.set_shader_param("pattern", pattern)
+
+
+func _set_is_inside(value: bool) -> void:
+    is_inside = value
+    _outline_material.set_shader_param("is_inside", is_inside)
+
+
+func _set_adds_margins(value: bool) -> void:
+    adds_margins = value
+    _outline_material.set_shader_param("adds_margins", adds_margins)
---
+++ src/texture_outlineable_animated_sprite.gd
@@ -0,0 +1,143 @@
+tool
+class_name TextureOutlineableAnimatedSprite, \
+"res://addons/scaffolder/assets/images/editor_icons/outlineable_animated_sprite.png"
+extends OutlineableAnimatedSprite
+## -   This supports toggling an outline around the sprite, and changing the[br]
+##     color of the outline.[br]
+## -   This implementation of OutlineableAnimatedSprite requires that you[br]
+##     provide two versions of the texture, one with the outline and one[br]
+##     without.[br]
+## -   TextureOutlineableSprite is more efficient than[br]
+##     ShaderOutlineableSprite.[br]
+##     -   By a factor of around x7, for a one-pixel border.[br]
+## -   But using a separate outline texture is more work for the sprite[br]
+##     author.[br]
+## -   So shader-based outlines might be better for prototyping, or when you[br]
+##     know you won't be rendering too many outlined sprites simultaneously.[br]
+
+
+export var normal_frames: SpriteFrames setget _set_normal_frames
+export var outlined_frames: SpriteFrames setget _set_outlined_frames
+
+# FIXME: --------------------------------------
+# - I copied `my_frame` from TextureOutlineableSprite.
+# - I'm not sure if the same problem happens with AnimatedSprite.
+# - If so, then we'll need to find another fix, probably by syncing the frame
+#   in _process().
+
+## -   NOTE: Animate on this field instead of frame!
+## -   This is an unfortunate hack.
+## -   But Godot's AnimationPlayer apparently bypasses the getter/setter for
+##     frame, so we cannot use set_frame to sync the outline sprite with the
+##     normal sprite.
+# export var my_frame: int setget _set_my_frame,_get_my_frame
+# func _set_my_frame(value) -> void:
+#     set_frame(value)
+# func _get_my_frame() -> int:
+#     return get_frame()
+
+var _configuration_warning := ""
+
+
+func _ready() -> void:
+    _update_inner_sprite()
+
+
+func _update_outline() -> void:
+    if is_outlined:
+        $Outline.frames = outlined_frames
+    else:
+        $Outline.frames = null
+    _set_outline_color(outline_color)
+    property_list_changed_notify()
+
+
+func _update_inner_sprite() -> void:
+    $Outline.centered = self.centered
+    $Outline.flip_h = self.flip_h
+    $Outline.flip_v = self.flip_v
+    $Outline.frame = self.frame
+    $Outline.offset = self.offset
+    $Outline.playing = self.playing
+    $Outline.animation = self.animation
+    $Outline.speed_scale = self.speed_scale
+
+
+func _set_configuration_warning(value: String) -> void:
+    _configuration_warning = value
+    update_configuration_warning()
+
+
+func _get_configuration_warning() -> String:
+    return _configuration_warning
+
+
+func _set_is_outlined(value: bool) -> void:
+    is_outlined = value
+    if !is_instance_valid(normal_frames):
+        _set_configuration_warning("normal_frames must be assigned.")
+        return
+    if !is_instance_valid(outlined_frames):
+        _set_configuration_warning("outlined_frames must be assigned.")
+        return
+    _update_outline()
+
+
+func _set_outline_color(value: Color) -> void:
+    outline_color = value
+    $Outline.modulate = outline_color
+
+
+func _set_outlined_frames(value: SpriteFrames) -> void:
+    outlined_frames = value
+    _update_outline()
+
+
+func _set_normal_frames(value: SpriteFrames) -> void:
+    set_frames(value)
+
+
+func set_centered(value: bool) -> void:
+    .set_centered(value)
+    _update_inner_sprite()
+
+
+func set_flip_h(value: bool) -> void:
+    .set_flip_h(value)
+    _update_inner_sprite()
+
+
+func set_flip_v(value: bool) -> void:
+    .set_flip_v(value)
+    _update_inner_sprite()
+
+
+func set_frame(value: int) -> void:
+    .set_frame(value)
+    _update_inner_sprite()
+
+
+func set_frames(value: SpriteFrames) -> void:
+    .set_frames(value)
+    _update_inner_sprite()
+    property_list_changed_notify()
+
+
+func set_offset(value: Vector2) -> void:
+    .set_offset(value)
+    _update_inner_sprite()
+
+
+func set_playing(value: bool) -> void:
+    .set_playing(value)
+    _update_inner_sprite()
+
+
+func set_speed_scale(value: float) -> void:
+    .set_speed_scale(value)
+    _update_inner_sprite()
+
+
+func set_animation(value: String) -> void:
+    .set_animation(value)
+    _update_inner_sprite()
