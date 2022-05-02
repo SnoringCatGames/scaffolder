dd8856eb37366d4f65fc5417d09e4e2a8e5ca78f
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-05-02 13:37:45 -0700

    Fix radial-menu center-area radius; Fix radial-menu-item hover-out transition.

--- src/gui/radial_menu/radial_menu.gd
+++ src/gui/radial_menu/radial_menu.gd
@@ -67,7 +67,7 @@ func open(position: Vector2) -> void:
     _center_area_control = ShapedLevelControl.new()
     _center_area_control.screen_radius = 0.0
     _center_area_control.shape_circle_radius = \
-            Sc.gui.hud.radial_menu_radius * Sc.level.camera.zoom.x * Sc.gui.scale
+            Sc.gui.hud.radial_menu_radius * Sc.gui.scale
     _center_area_control.connect("touch_up", self, "_on_center_area_touch_up")
     add_child(_center_area_control)
     Sc.level.level_control_press_controller.included_exclusively_control(
@@ -230,6 +230,8 @@ func _interpolate_openness(progress: float) -> void:
 func _interpolate_item_hover(
         item: RadialMenuItemData,
         progress: float) -> void:
+    item._hover_progress = progress
+    
     var scale: float = lerp(
             1.0, Sc.gui.hud.radial_menu_item_hovered_scale, progress)
     var radius: float = \
