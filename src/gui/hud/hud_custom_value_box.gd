fbdfcadd3f525fcbb5f8b4747bb2bcc3468b07ce
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-05-13 18:56:43 -0700

    Fix hud-key-value-list rescaling.

--- src/gui/hud/hud_custom_value_box.gd
+++ src/gui/hud/hud_custom_value_box.gd
@@ -7,5 +7,10 @@ func _ready() -> void:
     $HBoxContainer/HBoxContainer.add_child(item.create_control())
 
 
+func _on_gui_scale_changed() -> bool:
+    ._on_gui_scale_changed()
+    return false
+
+
 func _update_display() -> void:
     item.update_item()
