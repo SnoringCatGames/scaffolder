9eb22c017324ac76c500448789a7a6ad0438b958
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-07-22 08:16:14 -0700

    Start restructuring submodules.

---
+++ src/scaffolder/test_scaffolder_module.h
@@ -0,0 +1,18 @@
+#ifndef TEST_SCAFFOLDER_MODULE_H
+#define TEST_SCAFFOLDER_MODULE_H
+
+#ifdef SC_TESTS_ENABLED
+
+#include "scaffolder/scaffolder_module.h"
+
+#include "snore_core/internal/test_utils.h"
+
+namespace godot {
+
+// Add tests here when needed.
+
+} // namespace godot
+
+#endif // SC_TESTS_ENABLED
+
+#endif // TEST_SCAFFOLDER_MODULE_H
