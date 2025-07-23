f8c750500a07b0f9e74798ec0ca8035b207a33b2
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-07-23 10:58:33 -0700

    Continue restructuring submodules.

--- src/register_gdextension_types.cpp
+++ src/register_gdextension_types.cpp
@@ -1,7 +1,7 @@
 #include "register_gdextension_types.h"
 
-#include "snore_core/snore_core_main_module.h"
 #include "scaffolder/scaffolder_module.h"
+#include "snore_core/snore_core_main_module.h"
 
 #include <gdextension_interface.h>
 #include <godot_cpp/core/defs.hpp>
@@ -9,10 +9,9 @@
 
 using namespace godot;
 
-void initialize_scaffolder_gdextension_types(ModuleInitializationLevel p_level)
-{
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
-	{
+void initialize_scaffolder_gdextension_types(
+		ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 
@@ -21,10 +20,8 @@ void initialize_scaffolder_gdextension_types(ModuleInitializationLevel p_level)
 }
 
 void uninitialize_scaffolder_gdextension_types(
-	ModuleInitializationLevel p_level)
-{
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
-	{
+		ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 
@@ -32,25 +29,23 @@ void uninitialize_scaffolder_gdextension_types(
 	Scaffolder::unregister_gdextension_types(p_level);
 }
 
-extern "C"
-{
-	/**
-	 * This is the entry point of the GDExtension and will be called on
-	 * initialization. This function's name must be specified as the 'entry_symbol'
-	 * in the .gdextension file.
-	 */
-	GDExtensionBool GDE_EXPORT scaffolder_extension_init(
+extern "C" {
+/**
+ * This is the entry point of the GDExtension and will be called on
+ * initialization. This function's name must be specified as the 'entry_symbol'
+ * in the .gdextension file.
+ */
+GDExtensionBool GDE_EXPORT scaffolder_extension_init(
 		GDExtensionInterfaceGetProcAddress p_get_proc_address,
 		GDExtensionClassLibraryPtr p_library,
-		GDExtensionInitialization *r_initialization)
-	{
-		GDExtensionBinding::InitObject init_obj(
+		GDExtensionInitialization *r_initialization) {
+	GDExtensionBinding::InitObject init_obj(
 			p_get_proc_address, p_library, r_initialization);
-		init_obj.register_initializer(initialize_scaffolder_gdextension_types);
-		init_obj.register_terminator(uninitialize_scaffolder_gdextension_types);
-		init_obj.set_minimum_library_initialization_level(
+	init_obj.register_initializer(initialize_scaffolder_gdextension_types);
+	init_obj.register_terminator(uninitialize_scaffolder_gdextension_types);
+	init_obj.set_minimum_library_initialization_level(
 			MODULE_INITIALIZATION_LEVEL_SCENE);
 
-		return init_obj.init();
-	}
+	return init_obj.init();
+}
 }
